d25a1ec9be8f34be44648eda556ecb43e1ef42d6
Ujjwal <ujjwalagarwal863@gmail.com>
2024-05-17 17:20:15 +0530

    commit

--- Hand-Tracking.py
+++ Hand-Tracking.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+import time
 
 cap = cv2.VideoCapture(0)
 
@@ -7,6 +8,8 @@ mphands = mp.solutions.hands
 hands = mphands.Hands()
 mpDraw = mp.solutions.drawing_utils
 
+pTime = 0
+ctime = 0
 while True:
     success, img = cap.read()
 
@@ -17,6 +20,12 @@ while True:
     if results.multi_hand_landmarks :
         for handMarks in results.multi_hand_landmarks:
             mpDraw.draw_landmarks(img,handMarks,mphands.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img,"fps:" + str(int(fps)),(10,70),cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
